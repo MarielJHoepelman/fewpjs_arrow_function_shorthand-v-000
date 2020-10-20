4917392f5872a586fd6bd78dad99d204f015f716
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-10-20 20:13:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function divide(){
   return 2000/100
 }
-
+const divide = function () {
+  return 2000/100
+}
 const square = number => number * number
 
 const add = (param1, param2) => param1 + param2
